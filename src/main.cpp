839bf745a570004c7116998d1bacfbcdb1103c07
james <git@mus-ic.co.uk>
2026-03-14 17:39:15 +0000

    tidy up the containsKey bit (it was overcomplicated)

--- src/main.cpp
+++ src/main.cpp
@@ -121,8 +121,8 @@ std::vector<String> processData(String json) {
       
       char dirChar = getDirectionChar(t["direction"]);
       
-      const char* origin = t.containsKey("origin") && !t["origin"].isNull() ? (const char*)t["origin"] : "";
-      const char* dest = t.containsKey("destination") && !t["destination"].isNull() ? (const char*)t["destination"] : "";
+      const char* origin = t["origin"] | "";
+      const char* dest = t["destination"] | "";
       
       char buffer[17];
       snprintf(buffer, sizeof(buffer), "%c %s %.4s-%.4s", 
